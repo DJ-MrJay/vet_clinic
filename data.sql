23cd7e11d822a487251a924379efd9f45cea1c82
DJ-MrJay <97578860+DJ-MrJay@users.noreply.github.com>
2022-12-15 00:45:37 +0300

    Add queries

--- data.sql
+++ data.sql
@@ -11,16 +11,16 @@ INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_
 VALUES (4, 'Devimon', '2017-05-12', 5, true, 11);
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (5, 'Charmander', '2020-02-08', 0, false, 11);
+VALUES (5, 'Charmander', '2020-02-08', 0, false, -11);
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (6, 'Plantmon', '2021-11-15', 2, true, 5.7);
+VALUES (6, 'Plantmon', '2021-11-15', 2, true, -5.7);
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (7, 'Squirtle', '1993-04-02', 3, false, 12.13);
+VALUES (7, 'Squirtle', '1993-04-02', 3, false, -12.13);
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (8, 'Angemon', '2005-06-12', 1, true, 45);
+VALUES (8, 'Angemon', '2005-06-12', 1, true, -45);
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES (9, 'Boarmon', '2005-06-07', 7, true, 20.4);
--- queries.sql
+++ queries.sql
@@ -7,10 +7,51 @@ SELECT name FROM animals WHERE neutered = true;
 SELECT name FROM animals WHERE NOT name = 'Gabumon';
 SELECT name FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
-UPDATE animals
-SET species = 'digimon'
-WHERE name LIKE '%mon';
+-- Set the species column to unspecified
+BEGIN;
+UPDATE animals SET species = 'unspecified';
+SELECT species FROM animals;
+ROLLBACK;
+SELECT species FROM animals;
 
-UPDATE animals
-SET species = 'pokemon'
-WHERE species = '';
+-- Set the species column to digimon for all animals that have a name ending in mon.
+-- Set the species column to pokemon for all animals that don't have species already set.
+BEGIN;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+SELECT species FROM animals;
+
+-- Delete all records in the animals table, then roll back the transaction.
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+-- Delete all animals born after Jan 1st, 2022.
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT saved;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO saved;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT weight_kg FROM animals;
+
+-- How many animals are there?
+SELECT COUNT(*) FROM animals;
+
+-- How many animals have never tried to escape?
+SELECT COUNT(escape_attempts) FROM animals WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, AVG(escape_attempts) FROM animals GROUP BY neutered;
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
+
+-- What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2001-01-01' GROUP BY species;
