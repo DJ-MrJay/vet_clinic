41cedeaa9e46c55f765bb6630013f9c48f1bdc5f
DJ-MrJay <97578860+DJ-MrJay@users.noreply.github.com>
2022-12-15 12:51:32 +0300

    Modify schema

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE animals (
-  id int PRIMARY KEY,
+  id int,
   name varchar(100),
   date_of_birth date,
   escape_attempts int,
@@ -7,3 +7,22 @@ CREATE TABLE animals (
   weight_kg decimal,
   species varchar(100)
 );
+
+CREATE TABLE owners (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  full_name varchar(100),
+  age int
+);
+
+CREATE TABLE species (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  name varchar(100)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_ID INT REFERENCES species(id);
+ALTER TABLE animals ADD COLUMN owners_id INT REFERENCES owners(id);
+
+
+
