1e2b59bfecf89e7d8fd37d3dae1f6deccb9b8107
DJ-MrJay <97578860+DJ-MrJay@users.noreply.github.com>
2022-12-15 13:52:34 +0300

    Update schema

--- data.sql
+++ data.sql
@@ -1,32 +1,54 @@
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (1, 'Agumon', '2020-02-03', 0, true, 10.23);
+VALUES (1, 'Agumon', '2020-02-03', 0, true, 10.23),
+(2, 'Gabumon', '2018-11-15', 2, true, 8),
+(3, 'Pikachu', '2021-01-07', 1, false, 15.04),
+(4, 'Devimon', '2017-05-12', 5, true, 11),
+(5, 'Charmander', '2020-02-08', 0, false, -11),
+(6, 'Plantmon', '2021-11-15', 2, true, -5.7),
+(7, 'Squirtle', '1993-04-02', 3, false, -12.13),
+(8, 'Angemon', '2005-06-12', 1, true, -45),
+(9, 'Boarmon', '2005-06-07', 7, true, 20.4),
+(10, 'Blossom', '1998-10-13', 3, true, 17),
+(11, 'Ditto', '2022-05-14', 4, true, 22);
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (2, 'Gabumon', '2018-11-15', 2, true, 8);
+-- Insert data into owners table
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', 34),
+('Jennifer Orwell', 19),
+('Bob', 45),
+('Melody Pond', 77),
+('Dean Winchester', 14),
+('Jodie Whittaker', 38);
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (3, 'Pikachu', '2021-01-07', 1, false, 15.04);
+-- Modify owners_id column
+UPDATE animals
+SET owners_id = 1
+WHERE name = 'Agumon';
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (4, 'Devimon', '2017-05-12', 5, true, 11);
+UPDATE animals
+SET owners_id = 2
+WHERE name IN ('Pikachu', 'Gabumon');
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (5, 'Charmander', '2020-02-08', 0, false, -11);
+UPDATE animals
+SET owners_id = 3
+WHERE name IN ('Devimon', 'Plantmon');
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (6, 'Plantmon', '2021-11-15', 2, true, -5.7);
+UPDATE animals
+SET owners_id = 4
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (7, 'Squirtle', '1993-04-02', 3, false, -12.13);
+UPDATE animals
+SET owners_id = 5
+WHERE name IN ('Angemon', 'Boarmon');
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (8, 'Angemon', '2005-06-12', 1, true, -45);
+-- Insert data into species table
+INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (9, 'Boarmon', '2005-06-07', 7, true, 20.4);
+-- Modify species_id column
+UPDATE animals
+SET species_id = 2
+WHERE name LIKE '%mon';
 
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (10, 'Blossom', '1998-10-13', 3, true, 17);
-
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES (11, 'Ditto', '2022-05-14', 4, true, 22);
+UPDATE animals
+SET species_id = 1
+WHERE name NOT LIKE '%mon';
--- schema.sql
+++ schema.sql
@@ -5,24 +5,42 @@ CREATE TABLE animals (
   escape_attempts int,
   neutered boolean,
   weight_kg decimal,
-  species varchar(100)
 );
 
+ALTER TABLE animals
+ADD COLUMN species varchar(50);
+
 CREATE TABLE owners (
-  id int AUTO_INCREMENT PRIMARY KEY,
+  id int SERIAL PRIMARY KEY,
   full_name varchar(100),
   age int
 );
 
 CREATE TABLE species (
-  id int AUTO_INCREMENT PRIMARY KEY,
+  id int SERIAL PRIMARY KEY,
   name varchar(100)
 );
 
-ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals
+DROP COLUMN id;
 
-ALTER TABLE animals ADD COLUMN species_ID INT REFERENCES species(id);
-ALTER TABLE animals ADD COLUMN owners_id INT REFERENCES owners(id);
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN id SERIAL PRIMARY KEY;
+
+ALTER TABLE animals
+ADD COLUMN species_ID int;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owners_id int;
+
+ALTER TABLE animals
+ADD FOREIGN KEY (owner_id) REFERENCES owners(id);
 
 
 
